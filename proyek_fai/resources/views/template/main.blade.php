b188449649ee3cc80f8feb551d92d30441d48b3f
Surya Bumantara <bumantara.surya2213@gmail.com>
2021-11-05 16:53:51 +0700

    (+) design page login

---
+++ proyek_fai/resources/views/template/main.blade.php
@@ -0,0 +1,22 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>
+        @yield('title')
+    </title>
+</head>
+<body class="" style="background-color: #D6D6D6;">
+    @yield("navbar")
+    
+    @yield('main')
+
+    @yield('second')
+
+    @yield('footer')
+</body>
+</html>
